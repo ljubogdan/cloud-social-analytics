94079f5cf9da5b3c9951555ad378a10d192dddea
Vukasin Vujovic <vujovic.sv11.2023@uns.ac.rs>
2026-05-21 23:53:20 +0200

    Init project

---
+++ tests/unit/test_fdsf_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cloud_social_analytics.cloud_social_analytics_stack import CloudSocialAnalyticsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cloud_social_analytics/cloud_social_analytics_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CloudSocialAnalyticsStack(app, "cloud_social_analytics")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
