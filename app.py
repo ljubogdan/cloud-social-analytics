ad1e6960fdba56e748a5eea8844ea44a1049498d
Lazar Jovic <ljovic192@gmail.com>
2026-06-10 12:55:20 +0200

    Implemented lambda for processing twitter posts

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from aws_cdk import App, Environment
 from cloud_social_analytics_stacks.bronze_layer_stacks.data_stack.data_stack import DataStack
 from cloud_social_analytics_stacks.bronze_layer_stacks.function_stacks.twitter_fetcher_function_stack import TwitterFetcherFunctionStack
 from cloud_social_analytics_stacks.bronze_layer_stacks.function_stacks.hacker_news_fetcher_function_stack import HackerNewsFetcherFunctionStack
+from cloud_social_analytics_stacks.silver_layer_stacks.twitter_posts_function_stack import TwitterPostsSilverStack
 from cloud_social_analytics_stacks.silver_layer_stacks.twitters_users_function_stack import TwitterUsersSilverStack
 
 app = App()
@@ -28,6 +29,13 @@ hacker_news_function_stack = HackerNewsFetcherFunctionStack(
 twitter_users_silver_stack = TwitterUsersSilverStack(
     app,
     "SocialAnalyticsTwitterUsersSilverStack",
+    data_stack.data_lake,
+    env = env
+)
+
+twitter_posts_silver_stack = TwitterPostsSilverStack(
+    app,
+    "SocialAnalyticsTwitterPostsSilverStack",
     data_stack.data_lake,
     env = env
 )
---
+++ cloud_social_analytics_stacks/silver_layer_stacks/twitter_posts_function_stack.py
@@ -0,0 +1,45 @@
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_iam as iam,
+    Duration,
+    Size,
+)
+from constructs import Construct
+
+class TwitterPostsSilverStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, data_bucket, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        role = iam.Role(
+            self,
+            "TwitterPostsSilverRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+        )
+        
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"))
+        data_bucket.grant_read(role, "bronze/twitter/*")
+        data_bucket.grant_write(role, "silver/posts/*")
+
+        wrangler_layer = _lambda.LayerVersion.from_layer_version_arn(
+            self,
+            "WranglerLayer",
+            "arn:aws:lambda:eu-central-1:336392948345:layer:AWSSDKPandas-Python312:1"
+        )
+
+        fn = _lambda.Function(
+            self,
+            "TwitterPostsSilverLambda",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="twitter_posts_extraction.handler",
+            code=_lambda.Code.from_asset("lambdas/twitterPostsExtraction"),
+            role=role,
+            timeout=Duration.minutes(15),
+            memory_size=3008, 
+            ephemeral_storage_size=Size.gibibytes(10),
+            environment={
+                "BUCKET_NAME": data_bucket.bucket_name,
+            },
+            layers=[wrangler_layer],
+        )
