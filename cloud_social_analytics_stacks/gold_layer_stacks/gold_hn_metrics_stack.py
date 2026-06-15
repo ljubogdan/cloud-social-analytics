3ce01487398793837a2d1694c4f84c3cf2e53191
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-06-15 11:06:24 +0200

    gold stacks, need to register in app.py later

---
+++ cloud_social_analytics_stacks/gold_layer_stacks/gold_hn_metrics_stack.py
@@ -0,0 +1,53 @@
+from aws_cdk import (
+    Stack,
+    Duration,
+    aws_lambda as _lambda,
+    aws_iam as iam,
+)
+from constructs import Construct
+
+
+class GoldHnMetricsStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, data_bucket, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        role = iam.Role(
+            self,
+            "GoldHnMetricsRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+        )
+
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                "service-role/AWSLambdaBasicExecutionRole"
+            )
+        )
+
+        data_bucket.grant_read(role, "silver/*")
+        data_bucket.grant_write(role, "gold/*")
+
+        wrangler_layer = _lambda.LayerVersion.from_layer_version_arn(
+            self,
+            "WranglerLayer",
+            "arn:aws:lambda:eu-central-1:336392948345:layer:AWSSDKPandas-Python312:1"
+        )
+
+        fn = _lambda.Function(
+            self,
+            "GoldHnMetricsLambda",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="gold_hn_metrics.handler",
+            code=_lambda.Code.from_asset("lambdas/goldHnMetrics"),
+            role=role,
+            timeout=Duration.minutes(15),
+            memory_size=3008,
+            environment={
+                "BUCKET_NAME": data_bucket.bucket_name,
+            },
+            layers=[wrangler_layer],
+        )
+
+        fn.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.AWS_IAM,
+        )
