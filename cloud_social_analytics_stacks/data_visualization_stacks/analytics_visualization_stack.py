e97c884b2111a9b25e05b284c380ca343e7898e5
Lazar Jovic <ljovic192@gmail.com>
2026-07-01 10:03:31 +0200

    work

--- cloud_social_analytics_stacks/data_visualization_stacks/analytics_visualization_stack.py
+++ cloud_social_analytics_stacks/data_visualization_stacks/analytics_visualization_stack.py
@@ -2,166 +2,117 @@ from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_iam as iam,
-    aws_secretsmanager as secretsmanager,
     aws_lambda as _lambda,
-    Duration,
-    BundlingOptions
+    Duration
 )
 from constructs import Construct
 
+
 class AnalyticsVisualizationStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs):
-        super().__init__(scope, construct_id, **kwargs)
-        
-        vpc = ec2.Vpc.from_lookup(self, "DefaultVpc", is_default=True)
 
-        ec2.GatewayVpcEndpoint(
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
+
+        sg = ec2.SecurityGroup(
             self,
-            "S3Endpoint",
+            "AnalyticsSG",
             vpc=vpc,
-            service=ec2.GatewayVpcEndpointAwsService.S3
+            allow_all_outbound=True
         )
 
-        ec2_sg = ec2.SecurityGroup(self, "PostgresSG", vpc=vpc, allow_all_outbound=True)
-        lambda_sg = ec2.SecurityGroup(self, "LambdaSG", vpc=vpc, allow_all_outbound=True)
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22))
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8088))
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(5432))
 
-        ec2_sg.add_ingress_rule(lambda_sg, ec2.Port.tcp(5432), "Allow Lambda to access Postgres")
-
-        db_secret = secretsmanager.Secret(self, "PostgresPasswordSecret",
-            generate_secret_string=secretsmanager.SecretStringGenerator(
-                secret_string_template='{"username": "admin"}',
-                generate_string_key="password",
-                password_length=16
-            )
-        )        
-
-        role = iam.Role(self, "InstanceRole", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
-        
-        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
-        
-        role.add_to_policy(iam.PolicyStatement(
-            actions=["secretsmanager:GetSecretValue"],
-            resources=[db_secret.secret_arn]
-        ))
-
-        instance = ec2.Instance(self, "PostgresInstance",
-            instance_type=ec2.InstanceType("t3.micro"),
-            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
-            vpc=vpc,
-            security_group=ec2_sg,
-            role=role
-        )
-
-        instance.add_user_data(
-            "#!/bin/bash",
-            "yum update -y",
-            "yum install -y docker jq",
-            "systemctl start docker",
-            "systemctl enable docker",
-            "usermod -a -G docker ec2-user",
-            
-            "curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose",
-            "chmod +x /usr/local/bin/docker-compose",
-            
-            # f"SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id {db_secret.secret_arn} --region {self.region} --query SecretString --output text)",
-            #"DB_PASSWORD=$(echo $SECRET_JSON | jq -r .password)",
-            "DB_PASSWORD=mypassword123",
-
-            r"cat <<EOF > /home/ec2-user/docker-compose.yml",
-            "version: '3.8'",
-            "services:",
-            "  db:",
-            "    image: postgres:15-alpine",
-            "    container_name: postgres-db",
-            "    restart: always",
-            "    environment:",
-            "      POSTGRES_DB: socialanalytics",
-            "      POSTGRES_USER: admin",
-            r"      POSTGRES_PASSWORD: mypassword123",
-            "    ports:",
-            "      - '5432:5432'",
-            "    healthcheck:",
-            "      test: ['CMD-SHELL', 'pg_isready -U admin']",
-            "      interval: 5s",
-            "      timeout: 5s",
-            "      retries: 5",
-            "",
-            "  superset:",
-            "    image: apache/superset:latest",
-            "    container_name: superset-app",
-            "    restart: always",
-            "    ports:",
-            "      - '8088:8088'",
-            "    environment:",
-            "      - SUPERSET_SECRET_KEY=long-random-secret-key-change-me",
-            r"      - DATABASE_URL=postgresql://admin:$DB_PASSWORD@db:5432/socialanalytics",
-            "EOF",
-            
-            "cd /home/ec2-user",
-            "docker-compose up -d"
-        )
- 
-        lambda_role = iam.Role(
-            self, "LambdaExecutionRole",
+        role = iam.Role(
+            self,
+            "LambdaRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com")
         )
 
-        lambda_role.add_managed_policy(
+        role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(
                 "service-role/AWSLambdaBasicExecutionRole"
             )
         )
- 
-        lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess")
-        )
- 
-        db_secret.grant_read(lambda_role)
- 
-        lambda_role.add_to_policy(iam.PolicyStatement(
-            actions=[
-                "ec2:CreateNetworkInterface",
-                "ec2:DescribeNetworkInterfaces",
-                "ec2:DeleteNetworkInterface"
-            ],
+
+        role.add_to_policy(iam.PolicyStatement(
+            actions=["s3:ListBucket", "s3:GetObject"],
             resources=["*"]
         ))
- 
+
+        role.add_to_policy(iam.PolicyStatement(
+            actions=["ec2:DescribeInstances"],
+            resources=["*"]
+        ))
+
+        db_layer = _lambda.LayerVersion(
+            self,
+            "AnalyticsDbLayer",
+            code=_lambda.Code.from_asset("layers/analytics-db"),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_11]
+        )
+
         _lambda.Function(
             self,
-            "PostgresLoaderLambda",
+            "GoldToPostgresLambda",
             runtime=_lambda.Runtime.PYTHON_3_11,
-            handler="lambda_incremental_handler.lambda_incremental_postgres_handler",
-            code=_lambda.Code.from_asset(
-                "lambdas/analyticsVisualization",
-                bundling=BundlingOptions(
-                    image=_lambda.Runtime.PYTHON_3_11.bundling_image,
-                    command=[
-                        "bash",
-                        "-c",
-                        "cp -au . /asset-output"
-                    ]
-                )
-            ),
+            handler="lambda_incremental_handler.lambda_handler",
+            code=_lambda.Code.from_asset("lambdas/analyticsVisualization"),
             layers=[
+                db_layer,
                 _lambda.LayerVersion.from_layer_version_arn(
                     self,
-                    "AwsWranglerLayer",
-                    f"arn:aws:lambda:{self.region}:336392948345:layer:AWSSDKPandas-Python311:20"
+                    "AwsSdkPandasLayer",
+                    "arn:aws:lambda:eu-central-1:336392948345:layer:AWSSDKPandas-Python311:21"
                 )
             ],
-            #vpc=vpc,
-            allow_public_subnet=True,
-            #security_groups=[lambda_sg],
-            timeout=Duration.minutes(15),
+            timeout=Duration.minutes(10),
             memory_size=1024,
-            role=lambda_role,
+            role=role,
             environment={
-                "DB_HOST": instance.instance_private_ip,
-                "DB_USER": "admin",
-                "DB_NAME": "socialanalytics",
-                "BUCKET_NAME": "data-lake-bucket-social-analytics",
-                "SECRET_ARN": db_secret.secret_arn,
-                "DB_PASSWORD": "mypassword123"
+                "S3_BUCKET": "your-bucket",
+                "S3_PREFIX": "gold/",
+                "PG_CONN": "postgresql+pg8000://admin:admin123@<EC2_PUBLIC_IP>:5432/analytics"
             }
+        )
+
+        user_data = ec2.UserData.for_linux()
+
+        user_data.add_commands(
+            "yum update -y",
+            "yum install -y docker",
+            "service docker start",
+            "usermod -a -G docker ec2-user",
+
+            "curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+            "chmod +x /usr/local/bin/docker-compose",
+
+            "mkdir -p /opt/analytics",
+            "cd /opt/analytics",
+
+            "cat <<EOF > docker-compose.yml\nversion: '3.8'\nservices:\n  postgres:\n    image: postgres:15\n    environment:\n      POSTGRES_USER: admin\n      POSTGRES_PASSWORD: admin123\n      POSTGRES_DB: analytics\n    ports:\n      - '5432:5432'\n    volumes:\n      - pgdata:/var/lib/postgresql/data\n  superset:\n    image: apache/superset:latest\n    ports:\n      - '8088:8088'\n    environment:\n      SUPERSET_SECRET_KEY: 'my_secret_key'\n    depends_on:\n      - postgres\nvolumes:\n  pgdata:\nEOF",
+
+            "cd /opt/analytics",
+            "docker-compose up -d",
+
+            "sleep 30",
+            "docker exec superset superset db upgrade",
+            "docker exec superset superset fab create-admin --username admin --firstname admin --lastname admin --email admin@local.com --password admin",
+            "docker exec superset superset init"
+        )
+
+        ec2.Instance(
+            self,
+            "AnalyticsInstance",
+            vpc=vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.T3,
+                ec2.InstanceSize.MICRO
+            ),
+            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+            security_group=sg,
+            user_data=user_data,
         )
--- lambdas/analyticsVisualization/lambda_incremental_handler.py
+++ lambdas/analyticsVisualization/lambda_incremental_handler.py
@@ -1,105 +1,199 @@
-import awswrangler as wr
+import boto3
+import pandas as pd
+import io
 import os
 import logging
-from datetime import datetime, timezone, timedelta
+import re
+from collections import defaultdict
+from sqlalchemy import create_engine, MetaData, Table, Column
+from sqlalchemy.types import String, Integer, Float, DateTime, Boolean
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-print(os.listdir("/var/task"))
 
-def lambda_incremental_postgres_handler(event, context):
-    logger.info("START LAMBDA")
+S3_BUCKET = os.environ["S3_BUCKET"]
+S3_PREFIX = os.environ.get("S3_PREFIX", "gold/")
+PG_CONN = os.environ["PG_CONN"]
+s3 = boto3.client("s3")
 
-    try:
-        con = wr.postgresql.connect(
-            host=os.environ["DB_HOST"],
-            database=os.environ["DB_NAME"],
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"],
-            port=5432
-        )
 
-        yesterday = (datetime.now(timezone.utc).date() - timedelta(days=1)).strftime('%Y-%m-%d')
+def list_parquet_files():
+    logger.info("Listing S3 parquet files...")
 
-        partitioned_folders = {
-            "daily_hn_posts_metric",
-            "top_hn_users_high_karma",
-            "top_hn_users_low_karma",
-            "top_hn_posts_by_score",
-            "top_hn_jobs_by_score",
-            "data_quality_score"
-        }
+    paginator = s3.get_paginator("list_objects_v2")
 
-        mapping = {
-            "daily_hn_posts_metric": "hn_daily_posts",
-            "top_hn_users_high_karma": "hn_top_users_karma",
-            "top_hn_users_low_karma": "hn_bottom_users_karma",
-            "top_hn_posts_by_score": "hn_top_posts_score",
-            "top_hn_jobs_by_score": "hn_top_jobs_score",
-            "data_quality_score": "data_quality_report"
-        }
+    files = []
+    for page in paginator.paginate(Bucket=S3_BUCKET, Prefix=S3_PREFIX):
+        for obj in page.get("Contents", []):
+            if obj["Key"].endswith(".parquet"):
+                files.append(obj)
 
-        bucket_name = os.environ["BUCKET_NAME"]
+    logger.info(f"Found {len(files)} parquet files")
+    return files
 
-        successful_tables = []
-        failed_tables = []
 
-        for folder, table in mapping.items():
-            try:
-                base_path = f"s3://{bucket_name}/gold/{folder}/"
+def get_latest_per_metric(files):
+    grouped = defaultdict(list)
 
-                if folder in partitioned_folders:
-                    base_path += f"snapshot_date={yesterday}/"
+    for f in files:
+        key = f["Key"]
+        parts = key.split("/")
 
-                df = wr.s3.read_parquet(path=base_path)
+        if len(parts) < 2:
+            continue
 
-                logger.info(f"FOLDER: {folder}")
-                logger.info(f"BASE PATH: {base_path}")
+        metric = parts[1]
+        grouped[metric].append(f)
 
-                if df is None or df.empty:
-                    failed_tables.append(f"{folder} - empty")
-                    continue
+    latest = {}
+    for metric, items in grouped.items():
+        latest_file = max(items, key=lambda x: x["LastModified"])
+        latest[metric] = latest_file["Key"]
 
-                possible_keys = ["id", "post_id", "user_id", "username"]
-                subset_cols = [c for c in possible_keys if c in df.columns]
+    logger.info(f"Resolved latest files for {len(latest)} metrics")
+    return latest
 
-                if subset_cols:
-                    df = df.drop_duplicates(subset=subset_cols, keep="last")
-                    mode = "upsert"
-                else:
-                    df = df.drop_duplicates(keep="last")
-                    subset_cols = None
-                    mode = "append"
 
-                wr.postgresql.to_sql(
-                    df=df,
-                    con=con,
-                    table=table,
-                    schema="public",
-                    mode=mode,
-                    upsert_conflict_columns=subset_cols
-                )
+def read_parquet(key):
+    logger.info(f"Reading parquet: {key}")
 
-                successful_tables.append(table)
+    obj = s3.get_object(Bucket=S3_BUCKET, Key=key)
+    return pd.read_parquet(io.BytesIO(obj["Body"].read()))
 
-            except Exception as e:
-                failed_tables.append(f"{folder} - {str(e)}")
+
+POSSIBLE_KEYS = ["id", "post_id", "user_id", "username"]
+
+
+def detect_pk(df):
+    for k in POSSIBLE_KEYS:
+        if k in df.columns:
+            return k
+    return None
+
+
+def infer_sql_type(series):
+    if pd.api.types.is_integer_dtype(series):
+        return Integer()
+    if pd.api.types.is_float_dtype(series):
+        return Float()
+    if pd.api.types.is_bool_dtype(series):
+        return Boolean()
+    if pd.api.types.is_datetime64_any_dtype(series):
+        return DateTime()
+    return String()
+
+
+def normalize_pg_conn(conn_str):
+    if conn_str.startswith("postgresql+psycopg2://"):
+        return conn_str.replace("postgresql+psycopg2://", "postgresql+pg8000://", 1)
+    if conn_str.startswith("postgresql://"):
+        return conn_str.replace("postgresql://", "postgresql+pg8000://", 1)
+    if conn_str.startswith("postgres://"):
+        return conn_str.replace("postgres://", "postgresql+pg8000://", 1)
+    return conn_str
+
+
+def sanitize_table_name(name):
+    return re.sub(r"[^a-zA-Z0-9_]", "_", name)
+
+
+def create_table_if_not_exists(engine, table_name, df, pk):
+    metadata = MetaData()
+
+    columns = []
+    for col in df.columns:
+        col_type = infer_sql_type(df[col])
+
+        if col == pk:
+            columns.append(Column(col, col_type, primary_key=True))
+        else:
+            columns.append(Column(col, col_type))
+
+    table = Table(table_name, metadata, *columns)
+    metadata.create_all(engine)
+
+    logger.info(f"Table ensured: {table_name}")
+    return table
+
+
+def batch_upsert(engine, df, table_name, pk):
+    logger.info(f"Upserting {len(df)} rows into {table_name}")
+
+    cols = list(df.columns)
+    update_cols = [c for c in cols if c != pk]
+
+    rows = [tuple(x) for x in df.to_numpy()]
+
+    chunk_size = 1000
+
+    with engine.begin() as conn:
+        for i in range(0, len(rows), chunk_size):
+            batch = rows[i:i + chunk_size]
+
+            update_clause = ""
+            if update_cols:
+                update_clause = ",".join([f"{c}=EXCLUDED.{c}" for c in update_cols])
+
+            insert_sql = f"""
+                INSERT INTO {table_name} ({",".join(cols)})
+                VALUES ({",".join(["%s"] * len(cols))})
+                ON CONFLICT ({pk})
+                {"DO UPDATE SET " + update_clause if update_clause else "DO NOTHING"}
+            """
+
+            conn.exec_driver_sql(insert_sql, batch)
+
+            logger.info(f"Inserted batch {i + len(batch)}/{len(rows)}")
+
+    logger.info(f"Finished upsert for {table_name}")
+
+
+def lambda_handler(event, context):
+
+    logger.info("Lambda started")
+
+    engine = create_engine(normalize_pg_conn(PG_CONN), pool_pre_ping=True)
+
+    files = list_parquet_files()
+    latest_files = get_latest_per_metric(files)
+
+    results = []
+
+    for metric, key in latest_files.items():
+        try:
+            table_name = sanitize_table_name(metric)
+            logger.info(f"Processing metric: {metric} as table {table_name}")
+
+            df = read_parquet(key)
+
+            if df.empty:
                 continue
 
-        con.close()
+            pk = detect_pk(df)
 
-        return {
-            "statusCode": 200,
-            "status": "completed",
-            "successful_tables": successful_tables,
-            "failed_tables": failed_tables,
-            "timestamp": datetime.now(timezone.utc).isoformat()
-        }
+            if pk is None:
+                raise Exception(f"No primary key found for metric {metric}")
 
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "status": "failed",
-            "error": str(e),
-            "timestamp": datetime.now(timezone.utc).isoformat()
-        }
+            create_table_if_not_exists(engine, table_name, df, pk)
+
+            df = df.drop_duplicates(subset=[pk])
+
+            batch_upsert(engine, df, table_name, pk)
+
+            results.append({
+                "metric": table_name,
+                "file": key,
+                "rows": len(df),
+                "pk": pk
+            })
+
+        except Exception as e:
+            logger.error(f"Error processing {metric}: {str(e)}")
+            continue
+
+    logger.info("Lambda finished")
+
+    return {
+        "status": "success",
+        "processed": results
+    }
